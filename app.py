ddc0b177561b6f3cc733fe51b3d5b1a31da81a3c
Rezoneter <150760616+Rezoneter@users.noreply.github.com>
2023-12-19 14:57:34 +0900

    add

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_jwt_extended import JWTManager 
 from flask_restful import Api
 from config import Config
-from resources.posting import PostingListResource
+from resources.follow import FollowResource
+from resources.like import LikeResource
+from resources.posting import PostingListResource, PostingResource
 
 from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blocklist
 
@@ -25,6 +27,8 @@ api.add_resource(UserRegisterResource,'/user/register')
 api.add_resource(UserLoginResource,'/user/register')
 api.add_resource(UserLogoutResource,'/user/logout')
 api.add_resource(PostingListResource,'/posting')
-
+api.add_resource(FollowResource, '/follow/<int:followee_id>')
+api.add_resource(PostingResource, '/posting/<int:posting_id>')
+api.add_resource(LikeResource,'/like/<int:posting_id')
 if __name__ == '__main__':
     app.run()
